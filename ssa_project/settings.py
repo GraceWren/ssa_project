a53016b8f68473d0bda1ebc5835f8aedc05903e0
unknown <grace.wren@education.nsw.gov.au>
2024-10-29 14:39:49 +1100

    Added recaptcha v3

--- ssa_project/settings.py
+++ ssa_project/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+RECAPTCHA_SECRET_KEY = "6LeMRm4qAAAAAPslEmmSL7zQBpwLV-YHw0R99ytB"
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
